7b4bc42a3094a3852de582e1b9b50461b8e19c84
Ruan Joppert <ruan.joppert@gmail.com>
2024-03-23 21:53:34 -0300

    wip

--- middleware/otel/otel.go
+++ middleware/otel/otel.go
@@ -1,4 +1,4 @@
-package middleware
+package otel
 
 import (
 	"context"
@@ -9,9 +9,17 @@ import (
 
 func Otel[T any](h sbus.Handler, c sbus.PubsubConfig) sbus.HandleFunc[T] {
 	return func(ctx context.Context, i T) error {
-		ctx, span := otel.Tracer("").Start(ctx, h.Name())
+		ctx, span := otel.Tracer("opa").Start(ctx, h.Name())
 		defer span.End()
 
 		return h.Handle(ctx, i)
+	}
+}
+
+func WithGroup(g string) sbus.SubscribeOptionFunc {
+	return func(o sbus.PubsubConfig) sbus.PubsubConfig {
+		o.Metadata().Set("group", g)
+
+		return o
 	}
 }
