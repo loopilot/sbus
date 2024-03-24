1a95db363140c76a75c8440ee0f46a164601f02c
Ruan Joppert <ruan.joppert@gmail.com>
2024-03-23 22:18:49 -0300

    wip

--- middleware/otel/otel.go
+++ middleware/otel/otel.go
@@ -5,14 +5,44 @@ import (
 
 	"github.com/loopilot/sbus"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
 )
 
 func Otel[T any](h sbus.Handler, c sbus.PubsubConfig) sbus.HandleFunc[T] {
 	return func(ctx context.Context, i T) error {
-		ctx, span := otel.Tracer("opa").Start(ctx, h.Name())
-		defer span.End()
+		// when the handler has no group we create a new span linked to the parent span
+		if g, ok := c.Metadata().Get("group"); !ok {
+			link := trace.LinkFromContext(ctx)
+			ctxn := context.Background()
+			ctx, span := otel.Tracer("").Start(ctxn, h.Name(), trace.WithLinks(link))
+			defer span.End()
 
-		return h.Handle(ctx, i)
+			return h.Handle(ctx, i)
+		} else {
+			// when the handler has a group we check if the group matches the group in the context
+			group := g.(string)
+
+			ctxGroup, ok := ctx.Value("group").(string)
+
+			if !ok {
+				return nil
+			}
+
+			// if the groups match we create a new span linked to the parent span
+			if group == ctxGroup {
+				ctx, span := otel.Tracer("").Start(ctx, h.Name())
+				defer span.End()
+
+				return h.Handle(ctx, i)
+			} else {
+				link := trace.LinkFromContext(ctx)
+				ctxn := context.Background()
+				ctx, span := otel.Tracer("").Start(ctxn, h.Name(), trace.WithLinks(link))
+				defer span.End()
+
+				return h.Handle(ctx, i)
+			}
+		}
 	}
 }
 
